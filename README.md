c0bb550851938bca2d824ff2cdb009088f42cc36
Felipe Giraldo <felipe-giraldo@hotmail.es>
2021-08-25 23:25:52 -0500

    codigo del cuadrado

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Curso-Practico-JavaScript
+
+...
+
+## Taller # 1 : figuras geometricas
+
+- Primer paso: definir las formulas
+- Segundo paso: implementar la formulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
---
+++ figuras.js
@@ -0,0 +1,9 @@
+//codigo del cuadrado
+const ladoCuadrado = 5;
+console.log("el lado del cuadrado miden: " + ladoCuadrado + " cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("el perimetro del cuadrado es: " + perimetroCuadrado + " cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("el area del cuadro es: " + ladoCuadrado * ladoCuadrado + " cm^2");
